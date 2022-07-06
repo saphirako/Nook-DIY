dc27d1f971ea88dd2543826634b0f3730f086c08
Michael M <12539189+saphirako@users.noreply.github.com>
2022-07-06 01:34:08 -0400

    Fix color mismatch for light-gray

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import Tippy from "@tippyjs/react";
-import { Link } from "react-router-dom";
-import "tippy.js/dist/tippy.css";
+import React from 'react'
+import Tippy from '@tippyjs/react'
+import { Link } from 'react-router-dom'
+import 'tippy.js/dist/tippy.css'
 
 const mapACNHDataToTailwind = {
-    red: "bg-acnh-red",
-    yellow: "bg-acnh-yellow",
-    green: "bg-acnh-green",
-    blue: "bg-acnh-blue",
-    brick: "bg-acnh-brick",
-    light: "bg-acnh-light",
-    pink: "bg-acnh-pink",
-    beige: "bg-acnh-beige",
-    brown: "bg-acnh-brown",
-    orange: "bg-acnh-orange",
-    "dark-gray": "bg-acnh-dark-gray",
-    white: "bg-acnh-white",
-    gold: "bg-acnh-gold border-acnh-gold-border",
-    silver: "bg-acnh-silver border-acnh-silver-border",
-};
+    red: 'bg-acnh-red',
+    yellow: 'bg-acnh-yellow',
+    green: 'bg-acnh-green',
+    blue: 'bg-acnh-blue',
+    brick: 'bg-acnh-brick',
+    'light-gray': 'bg-acnh-light-gray',
+    pink: 'bg-acnh-pink',
+    beige: 'bg-acnh-beige',
+    brown: 'bg-acnh-brown',
+    orange: 'bg-acnh-orange',
+    'dark-gray': 'bg-acnh-dark-gray',
+    white: 'bg-acnh-white',
+    gold: 'bg-acnh-gold border-acnh-gold-border',
+    silver: 'bg-acnh-silver border-acnh-silver-border',
+}
 
 export default function SingleItem(props) {
     return (
@@ -30,14 +30,14 @@ export default function SingleItem(props) {
         >
             <Link
                 to={{
-                    pathname: "item",
+                    pathname: 'item',
                     search: props.itemData.name,
                     isShowing: props.isShowing,
                     transition: props.transition,
                 }}
-                state={{...props.itemData}}
+                state={{ ...props.itemData }}
                 className={
-                    "border-8 border-brown-100 shadow-recipecard rounded-xl w-44 relative bg-cover bg-diyrecipe transition-transform transform hover:-rotate-3 hover:scale-110 hover:z-50 " +
+                    'border-8 border-brown-100 shadow-recipecard rounded-xl w-44 relative bg-cover bg-diyrecipe transition-transform transform hover:-rotate-3 hover:scale-110 hover:z-50 ' +
                     mapACNHDataToTailwind[props.itemData.card_color]
                 }
                 onClick={(event) => props.focusMe(props.itemData, event)}
@@ -49,5 +49,5 @@ export default function SingleItem(props) {
                 />
             </Link>
         </Tippy>
-    );
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,75 +1,75 @@
 module.exports = {
-  content: [
-    "./src/**/*.html",
-    "./src/**/*.jsx",
-    "./src/**/*.tsx",
-    "./src/**/*.ts",
-  ],
-  theme: {
-    borderWidth: (theme) => ({
-      DEFAULT: "1px",
-      0: "0",
-      2: "2px",
-      3: "3px",
-      4: "4px",
-      6: "6px",
-      8: "8px",
-      12: "12px",
-    }),
-    fontFamily: {
-      sans: ["Nunito", "sans-serif"],
-    },
-    extend: {
-      backgroundImage: (theme) => ({
-        diyrecipe: "url('static/image/diyBackground.png')",
-      }),
-      boxShadow: {
-        recipecard: "0px 13px 10px -8px rgba(41,40,35,0.56)",
-      },
-      colors: {
-        highlight: "#13c5cb",
-        nookipedia: "#67af40",
-        brown: {
-          100: "#FFFBEE",
-          200: "#F0EBDA",
-          300: "#e6dfc4",
-          400: "#d3cda5",
-          500: "#D8C791",
-          DEFAULT: "#dfd7ac",
-          600: "#855d2d",
-          700: "#604320",
+    content: [
+        './src/**/*.html',
+        './src/**/*.jsx',
+        './src/**/*.tsx',
+        './src/**/*.ts',
+    ],
+    theme: {
+        borderWidth: (theme) => ({
+            DEFAULT: '1px',
+            0: '0',
+            2: '2px',
+            3: '3px',
+            4: '4px',
+            6: '6px',
+            8: '8px',
+            12: '12px',
+        }),
+        fontFamily: {
+            sans: ['Nunito', 'sans-serif'],
         },
-        "acnh-red": "#d8595a",
-        "acnh-yellow": "#e7da77",
-        "acnh-green": "#95d57c",
-        "acnh-blue": "#bad5eb",
-        "acnh-brick": "#bc9e6e",
-        "acnh-light": "#b9babe",
-        "acnh-pink": "#f9b9ba",
-        "acnh-beige": "#e5daac",
-        "acnh-brown": "#94693c",
-        "acnh-orange": "#f6c568",
-        "acnh-dark": "#6a6a6e",
-        "acnh-white": "#e8e8e6",
-        "acnh-gold": "#d6c574",
-        "acnh-gold-border": "#AE9846",
-        "acnh-silver": "#9e9fa3",
-        "acnh-silver-border": "#787B7A",
-      },
-      gridAutoRows: {
-        recipes: "15rem",
-      },
-      lineHeight: {
-        11: "2.75rem",
-        12: "3rem",
-      },
-      screens: {
-        hd: "1920px",
-      },
+        extend: {
+            backgroundImage: (theme) => ({
+                diyrecipe: "url('static/image/diyBackground.png')",
+            }),
+            boxShadow: {
+                recipecard: '0px 13px 10px -8px rgba(41,40,35,0.56)',
+            },
+            colors: {
+                highlight: '#13c5cb',
+                nookipedia: '#67af40',
+                brown: {
+                    100: '#FFFBEE',
+                    200: '#F0EBDA',
+                    300: '#e6dfc4',
+                    400: '#d3cda5',
+                    500: '#D8C791',
+                    DEFAULT: '#dfd7ac',
+                    600: '#855d2d',
+                    700: '#604320',
+                },
+                'acnh-red': '#d8595a',
+                'acnh-yellow': '#e7da77',
+                'acnh-green': '#95d57c',
+                'acnh-blue': '#bad5eb',
+                'acnh-brick': '#bc9e6e',
+                'acnh-light-gray': '#b9babe',
+                'acnh-pink': '#f9b9ba',
+                'acnh-beige': '#e5daac',
+                'acnh-brown': '#94693c',
+                'acnh-orange': '#f6c568',
+                'acnh-dark-gray': '#6a6a6e',
+                'acnh-white': '#e8e8e6',
+                'acnh-gold': '#d6c574',
+                'acnh-gold-border': '#AE9846',
+                'acnh-silver': '#9e9fa3',
+                'acnh-silver-border': '#787B7A',
+            },
+            gridAutoRows: {
+                recipes: '15rem',
+            },
+            lineHeight: {
+                11: '2.75rem',
+                12: '3rem',
+            },
+            screens: {
+                hd: '1920px',
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-};
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
